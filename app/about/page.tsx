1f1b44301e06ca707993a09a806a707929d39fe7
Adam Gonzalez <adam.gonz@outlook.com>
2023-03-02 20:33:43 -0800

    updated about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,39 +1,44 @@
-import { ArrowIcon, LinkIcon } from "components/icons";
-import Link from "next/link";
+import { ArrowIcon, LinkIcon } from "components/icons"
+import Link from "next/link"
 
 export const metadata = {
   title: "About ",
   description: "Freelance Web Developer.",
-};
+}
 
 export default function AboutPage() {
   return (
     <section>
-      <h1 className="font-bold text-3xl font-serif">About Me</h1>
-      <p className="my-5 text-neutral-800 dark:text-neutral-200">
-        Hey, I&apos;m Adam Gonzalez.
-      </p>
+      <h1 className="font-bold text-3xl font-serif">
+        Hey, I&apos;m Adam Gonzalez
+      </h1>
       <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
+        <br></br>
         <p>
-          I&apos;m currently a Freelance Developer! As a Freelance Web
-          Developer, my goal is to create visually stunning and vibrant
-          websites. I place a strong emphasis on ensuring the websites{" "}
-          <b>I design are both high-quality and user-friendly.</b> My aim is to
-          always meet my clients needs and exceed their expectations in the
-          final product.
+          I&apos;m currently a freelance web developer with a passion for
+          creating websites that are not only functional, but also visually
+          captivating and easy to use. My ultimate goal is to push the
+          boundaries of web design and create websites that truly stand out. To
+          me, web design is all about balance. I strive to find the perfect
+          balance between aesthetics and usability in all of my projects. Every
+          detail, from the color scheme to the typography, is carefully
+          considered to ensure that the final product meets my own high
+          standards of excellence.
         </p>
         <hr />
         <p>
-          My passion for creative pursuits, such as music, sports, movies, and
-          coding, ultimately led me to my role as a Freelance Web Developer.
+          I have <b>discovered</b> that my passion for creativity extends beyond
+          coding. I find joy in exploring a range of hobbies, including playing
+          music, practicing sports, watching movies, and, of course, coding.
         </p>
         <p>
-          I have a <b>deep love</b> for building for the web. My expertise
-          ranges from simple HTML files to complex React or Next.js
-          applications. The web is an amazing platform, as it allows anyone to
-          become a developer, writer, or creator without the need for
-          permission. The freedom to just build and create is what makes the web
-          so incredible.
+          Building for the web is my <b>passion</b>. From creating simple HTML
+          pages to developing complex React or Next.js applications, I find{" "}
+          <b>joy</b> in every step of the process. What I love most about the
+          web is its accessibility. It&apos;s a platform that allows anyone,
+          regardless of background or experience, to become a developer, writer,
+          or creator. The ability to freely build and create without any
+          barriers is what makes the web so amazing to me.
         </p>
         <p className="mb-8">
           In my free time, I love to engage in activities such as{" "}
@@ -41,8 +46,9 @@ export default function AboutPage() {
             playing softball, watching movies, and continuously expanding my
             knowledge as a developer
           </b>
-          . As a full-stack engineer, I am actively seeking a full-time role and
-          am dedicated to enhancing my development skills during my job search.
+          . As a full-stack engineer, I am actively seeking a full-time position
+          and am dedicated to enhancing my development skills during my career
+          search.
         </p>
         <div className="flex flex-col gap-2 md:flex-row md:gap-2">
           <Link
@@ -58,5 +64,5 @@ export default function AboutPage() {
         </div>
       </div>
     </section>
-  );
+  )
 }
