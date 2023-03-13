211d5a07658bc9448784c181cfa86751ab5456a4
Adam Gonzalez <adam.gonzo@outlook.com>
2023-03-12 23:48:27 -0700

    fixed the call

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,4 @@
-import Blogs from "@/components/blogs"
+import DevtoPosts from "@/components/blogs"
 
 export const metadata = {
   title: "About ",
@@ -9,7 +9,7 @@ export default function BlogsPage() {
   return (
     <section>
       <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
-        <Blogs />
+        <DevtoPosts />
       </main>
     </section>
   )
