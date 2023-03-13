e4745096124410dc584e477f0d2c7027d24080f2
Adam Gonzalez <adam.gonzo@outlook.com>
2023-03-13 02:58:51 -0700

    changed to update everytime

--- components/blogs.tsx
+++ components/blogs.tsx
@@ -9,7 +9,7 @@ const DevtoPosts = () => {
       .then((res) => res.json())
       .then((data) => setPosts(data))
       .catch((error) => console.error(error));
-  }, []);
+  });
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
