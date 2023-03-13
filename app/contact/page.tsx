9d7152244bab4c976a73a19773ced75123da1196
Adam Gonzalez <adam.gonzo@outlook.com>
2023-03-12 23:06:45 -0700

    changed email to add subject

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -57,7 +57,7 @@ export default function ContactPage() {
         <ArrowIcon />
       </a>
       <a
-        href="mailto:AdamGonzalez-Jr@outlook.com&subject=collaboration"
+        href="mailto:AdamGonzalez-Jr@outlook.com?subject=collaboration"
         className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
       >
         <div className="flex items-center">
