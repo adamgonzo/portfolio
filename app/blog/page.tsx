bc1851134ce60d586c6a54e337066d68806fd27a
Adam <86032875+adamgonzo@users.noreply.github.com>
2023-11-07 09:19:25 -0800

    Updating to fix blog post

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,7 +10,7 @@ export default function BlogsPage() {
   return (
     <section>
       <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
-        <BlogList />
+        <DevToPosts />
       </main>
     </section>
   )
