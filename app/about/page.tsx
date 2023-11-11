dbf3303d910eb5f4bf38c5d9cc7ce5d818e344d7
Adam <86032875+adamgonzo@users.noreply.github.com>
2023-11-10 17:30:08 -0800

    Updated About Section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,32 +6,25 @@ export const metadata = {
 export default function AboutPage() {
   return (
     <section>
-      <h1 className="font-bold text-3xl font-serif">
-        Hey, I&apos;m Adam Gonzalez
+      <h1 className="font-bold text-4xl mb-4 text-center">
+        Hey, I&apos;m Adam Gonzalez 👋🏼
       </h1>
-      <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
+      <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200 text-lg leading-relaxed">
         <br />
         <p>
-          As an aspiring web developer, I&apos;m eager to apply my passion for
+          As a Full Stack Developer, I&apos;m eager to apply my passion for
           creating functional yet visually appealing websites. While I&apos;m
           still working to master the complexities of web development, I aim to
-          eventually push boundaries and make sites that are truly exceptional.
-          For now, I&apos;m focused on honing my skills in order to build
-          websites that balance usability and aesthetics.
+          eventually push boundaries and make sites that are truly{' '}
+          <span className="font-bold">exceptional</span>. For now, I&apos;m
+          focused on honing my skills in order to build websites that balance
+          usability and aesthetics.
         </p>
         <hr />
         <p>
           I have <b>discovered</b> that my passion for creativity extends beyond
           coding. I find joy in exploring a range of hobbies, including playing
           music, practicing sports, watching movies, and, of course, coding.
-        </p>
-        <p>
-          As a new web developer, I&apos;m extremely <strong>passionate</strong>{' '}
-          about building for the web. I find <strong>joy</strong> in each step
-          of the development process, from writing HTML to coding complex
-          frameworks like React. What excites me most is the web&apos;s
-          accessibility - it allows anyone to become a creator, regardless of
-          their background.
         </p>
         <p className="mb-8">
           In my free time, I love to engage in activities such as{' '}
