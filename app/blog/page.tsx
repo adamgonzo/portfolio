882d39ec02d8cb56da3410b30c192ad104494fb4
Adam <86032875+adamgonzo@users.noreply.github.com>
2024-01-31 13:00:42 -0800

    took space away from function Views
    Needed to clean up space for views

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -46,6 +46,5 @@ export default function BlogPage() {
 
 async function Views({ slug }: { slug: string }) {
   let views = await getViewsCount()
-
   return <ViewCounter allViews={views} slug={slug} trackView={false} />
 }
